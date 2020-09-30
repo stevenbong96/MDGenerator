6048dca2ddb381cd3a9c258f6c1f5398ca073cec
Steven Bong <stevenseal99@yahoo.com>
2020-09-29 18:36:31 -0700

    Add files from class repo this HW repo. Worked on stating the questions to the users for generating the README.md file. Tested the node to see if it works

---
+++ READMEGenerator.md
@@ -0,0 +1,13 @@
+{
+  "projectTitle": "Hello",
+  "tableContents": "How",
+  "description": "Are",
+  "installation": "You",
+  "usage": "Testing",
+  "contribution": "None",
+  "test": "None",
+  "license": "MIT License",
+  "username": "steven",
+  "githubRepo": "github.com",
+  "methodCommunication": "Email"
+}
--- index.js
+++ index.js
@@ -5,14 +5,96 @@ var fsFile = require("fs");
 // Declare the prompt for users
 inquirer.prompt([
     {
-
-    }
+        name:"projectTitle",
+        type:"input",
+        message:"What is your project title?"
+    },
+    {
+        name:"tableContents",
+        type:"input",
+        message:"Put your table of contents"
+    },
+    {
+        name:"description",
+        type:"input",
+        message:"Describe your project"
+    },
+    {
+        name:"installation",
+        type:"input",
+        message:"Provide installation instructions"
+    },
+    {
+        name:"usage",
+        type:"input",
+        message:"Provide usage information"
+    },
+    {
+        name:"contribution",
+        type:"input",
+        message:"Provide contribution guidelines"
+    },
+    {
+        name:"test",
+        type:"input",
+        message:"Provide test instructions"
+    },
+    {
+        name:"license",
+        type:"list",
+        message:"Select your preferred license",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]
+    },
+    {
+        name:"username",
+        type:"input",
+        message:"What is your Github username?"
+    },
+    {
+        name:"githubRepo",
+        type:"input",
+        message:"What is your Github repo link?"
+    },
+    {
+        name:"githubRepo",
+        type:"input",
+        message:"What is your email address?"
+    },
+    {
+        name:"methodCommunication",
+        type:"list",
+        message:"What is your preferred method of communciation?",
+        choices:["Email", "Text Message", "Phone"]
+    },
 ]).then(function(response){
     console.log(response);
-    fsFile.writeFile("README.md", JSON.stringify(response), function(err){
+    fsFile.writeFile("READMEGenerator.md", JSON.stringify(response,null,2), function(err){
         if(err){
             throw err
         }
         console.log("Success!!");
     })
 })
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    // fsFile.writeFile(fileName, JSON.stringify(data), function(err){
+    //     if(err){
+    //         throw err
+    //     }
+    //     console.log("Success!!");
+    // })
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
