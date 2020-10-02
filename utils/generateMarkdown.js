81f1d74d4e5771936eb5d0e911d8e09a4046495f
Steven Bong <stevenseal99@yahoo.com>
2020-10-01 17:21:30 -0700

    Pulled to github to check README template. Fixed the identation in .md file so it shows like the actual README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,40 +10,41 @@ function generateMarkdown(response) {
   //   }
   // }
   
-  return `# ${response.title}
+  return `
+  # ${response.title}
 
-    ## Description 
-    ${response.description}
+  ## Description 
+  ${response.description}
     
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Credits](#credits)
-    * [License](#license)
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
 
-    ## Installation 
-    ${response.installation}
+  ## Installation 
+  ${response.installation}
     
-    ## Usage 
-    ${response.usage}
+  ## Usage 
+  ${response.usage}
     
-    ## Contributions
-    [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md) 
+  ## Contributions
+  [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md) 
     
-    https://img.shields.io/github/contributors/${response.username}/${response.githubRepo}
-    ${response.contribution}
+  https://img.shields.io/github/contributors/${response.username}/${response.githubRepo}
+  ${response.contribution}
 
-    ## Test Instruction  
-    ${response.testinstruction}
+  ## Test Instruction  
+  ${response.testinstruction}
 
-    ## License 
-    ![Badge for License](https://img.shields.io/github/${response.license}/${response.username}/${response.githubRepo}?color=green)
+  ## License 
+  ![Badge for License](https://img.shields.io/github/${response.license}/${response.username}/${response.githubRepo}?color=green)
 
-    ## Profile 
-    ![Github Profile Picture] ${response.picture}
-    Github username: @${response.username}
+  ## Profile 
+  ![Github Profile Picture] ${response.picture}
+  Github username: @${response.username}
 
-    If you have any questions, feel free to ${response.methodCommunication} at ${response.email}
+  If you have any questions, feel free to ${response.methodCommunication} at ${response.email}
   `;
 
 }
